182de386a358f808fb0053a85fe84acdf1ccb663
salgado93 <dsalgado93@outlook.com>
2020-08-07 01:51:42 -0600

    Updates

---
+++ precache-manifest.0d295968349a4e7debee6c6d729c130f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b0921f6ba89d8ec3de52bb02daa913e",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "3d89648091db76b505e1",
+    "url": "/currency-converter/static/css/main.17075319.chunk.css"
+  },
+  {
+    "revision": "7365d83fff62cb0af6f5",
+    "url": "/currency-converter/static/js/2.edb56e9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-converter/static/js/2.edb56e9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d89648091db76b505e1",
+    "url": "/currency-converter/static/js/main.5304d594.chunk.js"
+  },
+  {
+    "revision": "a0730edf8381b13d6dc3",
+    "url": "/currency-converter/static/js/runtime-main.46121f8e.js"
+  }
+]);
